be9e1c617b7962ae14a21a3987b8e6b8cc8d8256
Marcelo Ferreira <marcelo.msgf.pro@gmail.com>
2024-12-06 22:59:53 -0300

    ""

--- xUnitDemo.UnitTests/CharacterTests.cs
+++ xUnitDemo.UnitTests/CharacterTests.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using xUnitDemo.Classes;
 
 namespace xUnitDemo.UnitTests.VS
 {
-    internal class CharacterTests
+    public class CharacterTests
     {
+        [Fact]
+        public void TakeDamage_DamageMinor0_returnException()
+        {
+            //arranje
+            var character = new Character("Link", 100);
+
+            //assert
+            Assert.Throws<ArgumentException>(() => character.TakeDamage(-10));
+        }
 
     }
 }
--- xUnitDemo.UnitTests/InventoryTests.cs
+++ xUnitDemo.UnitTests/InventoryTests.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using xUnitDemo.Classes;
 
 namespace xUnitDemo.UnitTests.VS
 {
-    internal class InventoryTests
+    public class InventoryTests
     {
+        [Fact]
+        public void AddItem_WhenItemIsNull_ShouldThrowArgumentException()
+        {
+            // Arrange
+            var inventory = new Inventory(3);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => inventory.AddItem(null));
+        }
+
+        [Fact]
+        public void AddItem_WhenItemIsEmpty_ShouldThrowArgumentException()
+        {
+            // Arrange
+            var inventory = new Inventory(3);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => inventory.AddItem(string.Empty));
+        }
+
+        [Fact]
+        public void AddItem_WhenCapacityIsExceeded_ShouldReturnFalse()
+        {
+            // Arrange
+            var inventory = new Inventory(2);
+            inventory.AddItem("Item1");
+            inventory.AddItem("Item2");
+
+            // Act
+            var result = inventory.AddItem("Item3");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AddItem_WhenItemIsAddedSuccessfully_ShouldReturnTrue()
+        {
+            // Arrange
+            var inventory = new Inventory(3);
+
+            // Act
+            var result = inventory.AddItem("NewItem");
+
+            // Assert
+            Assert.True(result);
+        }
+      
     }
 }
